2b26e87b1088da02d1803411059b97e966968c9b
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:24:57 +0800

    Determine the quotient

--- Program_05.py
+++ Program_05.py
@@ -1,3 +1,5 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 num_01 = int(input("Enter your first number: "))
 num_02 = int(input("Enter your second number: "))
+quotient = num_01 / num_02
+print(f"The quotient of both numbers, {num_01} and {num_02}, is {quotient}!")
