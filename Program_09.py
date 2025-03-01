afc436c079d7cd197cc3c0bc9796d2b3d4e36427
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:39:31 +0800

    Display even numbers

--- Program_09.py
+++ Program_09.py
@@ -3,3 +3,6 @@ even_numbers = []
 for i in range (101):
     if i % 2 == 0:
         even_numbers.append(i)
+print("The following are all even numbers starting from 0 to 100:\n")
+for j in even_numbers:
+    print(j)
