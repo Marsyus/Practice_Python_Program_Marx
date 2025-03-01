5afa9a330adaa827c0639ef49ec1ec33b2c7ac1b
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:41:00 +0800

    Append numbers not ending with 0

--- Program_10.py
+++ Program_10.py
@@ -1,2 +1,5 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 numbers = []
+for i in range (101):
+    if i % 10:
+        numbers.append(i)
