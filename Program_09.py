901cf86d8c0ad25118f5bffae20dec3181e47b28
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 23:31:18 +0800

    Removed list for simplicity

--- Program_09.py
+++ Program_09.py
@@ -1,8 +1,5 @@
 #Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
-even_numbers = []
+print("The following are all even numbers starting from 0 to 100:\n")
 for i in range (101):
     if i % 2 == 0:
-        even_numbers.append(i)
-print("The following are all even numbers starting from 0 to 100:\n")
-for j in even_numbers:
-    print(j)
+        print(i)
