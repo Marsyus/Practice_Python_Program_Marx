943021e594f75ad207bf9ef972b7c36e2bacb0ed
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:36:28 +0800

    Determine the count of odd numbers

--- Program_08.py
+++ Program_08.py
@@ -4,3 +4,7 @@ odd_count = 0
 for i in range(1, 11):
     num = int(input(f"Enter number {i}: "))
     numbers.append(num)
+for j in numbers:
+    if j % 2:
+        odd += 1
+print(f"The count of odd numbers from all the 10 numbers is {odd_count}!")
