5f1e9d7fb8bdf0f71eeb72bc871e21c61a2f3bbf
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:23:36 +0800

    Determine the product

--- Program_04.py
+++ Program_04.py
@@ -1,3 +1,5 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 num_01 = int(input("Enter your first number: "))
 num_02 = int(input("Enter your second number: "))
+product = num_01 * num_02
+print(f"The product of both numbers, {num_01} and {num_02}, is {product}!")
