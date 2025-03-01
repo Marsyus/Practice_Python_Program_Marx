a62a732b8983c8198eeb7bdef459a6c04769cb0f
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 23:33:48 +0800

    Removed list for simplicity

--- Program_10.py
+++ Program_10.py
@@ -1,8 +1,5 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
-numbers = []
+print("The following are all numbers except numbers ending in 0:\n")
 for i in range (101):
     if i % 10:
-        numbers.append(i)
-print("The following are all numbers except numbers ending in 0:\n")
-for j in numbers:
-    print(j)
+        print(i)
