182c9ac44878aa1d52e10b9a81994e4038c5b23b
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:11:47 +0800

    Determine if equal

--- Program_02.py
+++ Program_02.py
@@ -1,3 +1,5 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 num_01 = input("Enter your first number: ")
 num_02 = input("Enter your second number: ")
+if num_01 == num_02:
+    print(f"\nBoth numbers, {num_01} and {num_02}, are Equal!")
