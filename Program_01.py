613879120c2564140bdd46c46e1255c82f784512
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:08:00 +0800

    Determine which is bigger

--- Program_01.py
+++ Program_01.py
@@ -2,3 +2,7 @@
 
 num_01 = input("Enter your first number: ")
 num_02 = input("Enter your second number: ")
+if num_01 > num_02:
+    print(f"\nThe first number, {num_01}, is larger!")
+elif num_01 < num_02:
+    print(f"\nThe second number, {num_02}, is larger!")
