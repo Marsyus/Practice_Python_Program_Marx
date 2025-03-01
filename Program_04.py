f260633108a7d7765d6962f744ef3c89eaafa4bd
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:22:19 +0800

    Input numbers

--- Program_04.py
+++ Program_04.py
@@ -1,1 +1,3 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+num_01 = int(input("Enter your first number: "))
+num_02 = int(input("Enter your second number: "))
--- Program_05.py
+++ Program_05.py
@@ -1,1 +1,3 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+num_01 = int(input("Enter your first number: "))
+num_02 = int(input("Enter your second number: "))
