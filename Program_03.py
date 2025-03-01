d0b9c0ba77ded0d5779f9bceae61540996f8d82e
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:15:26 +0800

    Determine the sum

--- Program_03.py
+++ Program_03.py
@@ -1,3 +1,5 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 num_01 = int(input("Enter your first number: "))
 num_02 = int(input("Enter your second number: "))
+add = num_01 + num_02
+print(f"The sum of both numbers, {num_01} and {num_02}, is {add}!")
