8ca2f5c8d676e41b306fc36d0e555d07a6346ecb
Marsyus <miadolomboy0917@gmail.com>
2025-03-01 13:14:15 +0800

    Input numbers

--- Program_03.py
+++ Program_03.py
@@ -1,1 +1,3 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+num_01 = int(input("Enter your first number: "))
+num_02 = int(input("Enter your second number: "))
